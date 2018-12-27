fd239a51fd9d4cb0dc266a9bb8a6d034d8de16ad
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 10:41:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+  i = 0 
+  new_arr = []
+  while i < collection.length 
+    if yield(arr[i])
+      new_arr << arr[i]
+    end
+  end
+  
+  return new_arr
 end
